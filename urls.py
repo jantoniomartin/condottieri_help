90262b9b1d9a40f9300e986ed63dbe163cdbba6c
José Antonio Martín Prieto <jantonio.martin@gmail.com>
2019-09-08 23:07:18 +0200

    removed django.conf.url.patterns

--- urls.py
+++ urls.py
@@ -1,10 +1,9 @@
-from django.conf.urls import *
+from django.conf.urls import url
 from django.views.generic.base import TemplateView
 
 from condottieri_help.views import HelpTemplateView
 
-urlpatterns = patterns('condottieri_help.views',
+urlpatterns = [
     url(r'^$', HelpTemplateView.as_view(), name='help-index'),
     url(r'^contribute$', TemplateView.as_view(template_name='condottieri_help/contribute.html'), name='help-contribute'),
-)
-
+]
